9c80962b119fba6e3665b580ffdbc7223017491e
Clement Ndimuangu <clement.ndimuangu@verizon.com>
2024-09-11 13:45:57 -0500

    initial commit

--- index.js
+++ index.js
@@ -4,15 +4,8 @@ const { SalesPerson } = require("./classes/SalesPerson")
 const { SoftwareEngineer } = require("./classes/SoftwareEngineer")
 
 // Run and debug your code here
-const preston = new Employees("Preston", "Engineer", 100000);
-preston.getSalary(); // 100000
-preston.setSalary(105000);
-preston.getSalary(); // 105000
-preston.getStatus(); // true;
-preston.setStatus("fire");
-preston.getStatus(); // false;
 
-console.log(preston.getStatus());
+
 
 
 
